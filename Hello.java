855689faeab417ba49a9406b0fc481f3c1be6af5
Aniruddha khandait <samyakkhandait@gmail.com>
2026-01-24 20:22:17 +0530

    commit the new changes

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Main {
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
+        System.out.print("Hello world ");
 
-      system.out.print("Hello world")
-
+        System.out.print(" we start out journey here to learn java");
     }
 }
