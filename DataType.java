2db3a085fb0d4fc3da3651edffd2bbd496380274
Aniruddha khandait <samyakkhandait@gmail.com>
2026-02-05 16:38:21 +0530

    Add Datatype file: update the file structure

--- DataType.java
+++ DataType.java
@@ -1,26 +1,27 @@
 public class Datatype {
 
     public static void main(String[] args) {
-        // primative datatypes
-        //
-        //         int age = 30;
-        //
-        //         double year = 1.2;
-        //         double price = 19.99;
-        //
-        //         char grade = 'A';
-        //
-        //         boolean isStudent= true;
-        //         boolean forSale = false;
-        //
-        //        System.out.println("My age is :" + age);
-        //        System.out.println("we complete " +year+ " year");
-        //        System.out.println("$ " + price);
-        //        System.out.println("we got " + grade + " In last project");
-        //
-        //        System.out.println(isStudent);
+        // This are  primative datatypes
+        // contain : int | double | char | boolean
+        int age = 30;
 
-        // refrerance datatype
+        double year = 1.2;
+        double price = 19.99;
+
+        char grade = 'A';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+
+        System.out.println("My age is :" + age);
+        System.out.println("we complete " + year + " year");
+        System.out.println("$ " + price);
+        System.out.println("we got " + grade + " In last project");
+
+        System.out.println(isStudent);
+
+        // This are refrerance datatype
+        // contain : String | array | objects
 
         String name = "nick john";
         String car = "Mustang";
