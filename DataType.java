dec38263cd66aed24f2599024d2b9363038b985d
Aniruddha khandait <samyakkhandait@gmail.com>
2026-02-05 16:36:39 +0530

    update

---
+++ DataType.java
@@ -0,0 +1,31 @@
+public class Datatype {
+
+    public static void main(String[] args) {
+        // primative datatypes
+        //
+        //         int age = 30;
+        //
+        //         double year = 1.2;
+        //         double price = 19.99;
+        //
+        //         char grade = 'A';
+        //
+        //         boolean isStudent= true;
+        //         boolean forSale = false;
+        //
+        //        System.out.println("My age is :" + age);
+        //        System.out.println("we complete " +year+ " year");
+        //        System.out.println("$ " + price);
+        //        System.out.println("we got " + grade + " In last project");
+        //
+        //        System.out.println(isStudent);
+
+        // refrerance datatype
+
+        String name = "nick john";
+        String car = "Mustang";
+
+        System.out.println(name);
+        System.out.println(car);
+    }
+}
